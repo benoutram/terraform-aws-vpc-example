e068cdb54bd8e285e5834623f9dc7eaca5b7155d
Ben Outram <ben@hiveit.co.uk>
2018-01-11 16:26:09 +0000

    Moving key to web.tf and using variables for autoscaling group size.

--- project.tf
+++ project.tf
@@ -9,12 +9,6 @@ provider "template" {
   version = "~> 1.0"
 }
 
-# Create a key pair that will be assigned to our instances.
-resource "aws_key_pair" "deployer" {
-  key_name   = "terraform_deployer"
-  public_key = "${file(var.public_key_path)}"
-}
-
 # Create a VPC to launch our instances into.
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
--- web.tf
+++ web.tf
@@ -1,3 +1,9 @@
+# Create a key pair that will be assigned to our instances.
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform_deployer"
+  public_key = "${file(var.public_key_path)}"
+}
+
 # Create a new EC2 launch configuration to be used with the autoscaling group.
 resource "aws_launch_configuration" "launch_config" {
   name_prefix                 = "terraform-example-web-instance"
@@ -17,8 +23,8 @@ resource "aws_launch_configuration" "launch_config" {
 # Create the autoscaling group.
 resource "aws_autoscaling_group" "autoscaling_group" {
   launch_configuration = "${aws_launch_configuration.launch_config.id}"
-  min_size             = 3
-  max_size             = 10
+  min_size             = "${var.autoscaling_group_min_size}"
+  max_size             = "${var.autoscaling_group_max_size}"
   target_group_arns    = ["${aws_alb_target_group.group.arn}"]
   vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
 
