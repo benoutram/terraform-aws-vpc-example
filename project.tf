aea48776a9a1a6019cd04ed81ca389b9c5170a22
Ben Outram <ben@hiveit.co.uk>
2018-01-18 16:40:22 +0000

    Initial import for Lab part 1

---
+++ project.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  version    = "~> 0.1"
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "public_key_path" {}
+variable "certificate_arn" {}
+variable "route53_hosted_zone_name" {}
