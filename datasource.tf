546070328684cfdef8cb2127553ba1bb3173d5ff
Ben Outram <ben@hiveit.co.uk>
2018-01-18 16:43:38 +0000

    Initial import for Lab part 2

---
+++ datasource.tf
@@ -0,0 +1,2 @@
+# Create a data source for the availability zones.
+data "aws_availability_zones" "available" {}
--- project.tf
+++ project.tf
@@ -4,3 +4,60 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
+
+# Create a VPC to launch our instances into.
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name = "terraform-example-vpc"
+  }
+}
+
+# Create an internet gateway to give our subnet access to the outside world.
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "terraform-example-internet-gateway"
+  }
+}
+
+# Grant the VPC internet access on its main route table.
+resource "aws_route" "route" {
+  route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gateway.id}"
+}
+
+# Create subnets in each availability zone to launch our instances into, each with address blocks within the VPC.
+resource "aws_subnet" "main" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+
+  tags {
+    Name = "public-${element(data.aws_availability_zones.available.names, count.index)}"
+  }
+}
+
+# Create a security group in the VPC which our instances will belong to.
+resource "aws_security_group" "default" {
+  name        = "terraform_security_group"
+  description = "Terraform example security group"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  # Allow outbound internet access.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "terraform-example-security-group"
+  }
+}
